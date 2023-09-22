9fc33da83c187a818f25cb9bb331d02be41baaa3
codarsssss <codarsssss@gmail.com>
2023-09-22 21:54:10 +0300

    fix view model

--- test_hq/courses_app/models.py
+++ test_hq/courses_app/models.py
@@ -29,14 +29,16 @@ class Lesson(models.Model):
 
 
 class View(models.Model):
-    class Status(models.TextChoices):
-        VIEWED = 'Да', 'Просмотрено'
-        NOT_VIEWED = 'Нет', 'Не просмотрено'
+    def status_view(self):
+        if self.view_time > self.lesson.duration * 1.8:
+            return 'Да', 'Просмотрено'
+        else:
+            return 'Нет', 'Не просмотрено'
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
     view_time = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=3, choices=Status.choices, default=Status.NOT_VIEWED)
+    status = models.CharField(max_length=3, default=status_view)
 
     class Meta:
         ordering = ['-view_time']
--- test_hq/courses_app/views.py
+++ test_hq/courses_app/views.py
@@ -9,36 +9,10 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-
 class LessonViewSet(viewsets.ModelViewSet):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
-
 class ViewViewSet(viewsets.ModelViewSet):
     queryset = View.objects.all()
     serializer_class = ViewSerializer
-
-    def __init__(self, request, *args, **kwargs):
-
-        super().__init__(**kwargs)
-        data = request.data
-
-        lesson_id = data.get('lesson')
-        lesson = Lesson.objects.get(id=lesson_id)
-
-        duration = lesson.duration
-
-        viewed_time = data.get('view_time')
-
-        viewed_percentage = (viewed_time / duration) * 100
-
-        if viewed_percentage >= 80:
-            data['status'] = 'Да'
-        else:
-            data['status'] = 'Нет'
-
-        # Создаем запись о просмотре урока
-        serializer = self.get_serializer(data=data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
